16a0285e8081c78bbc3592831b30520b65c32e87
voutasaurus <voutasaurus@gmail.com>
2015-11-21 21:25:45 -0800

    Parallelizes the processing step

--- main.go
+++ main.go
@@ -2,58 +2,100 @@ package main
 
 import (
 	"bufio"
-	"errors"
 	"fmt"
 	"os"
+	"sync"
 )
 
-func main() {
-	orchestrateStreaming()
+type wordMember struct {
+	inWord  string
+	outWord string
 }
 
-func orchestrateStreaming() {
-	from := "wiki-100k.txt"
-	to := "words.go"
-	err := streamWords(from, to)
-	if err != nil {
-		fmt.Println("Stream failed:", err)
-	}
-}
+func transcribe(inFile, outFile string) error {
 
-func streamWords(fromFilename, toFilename string) error {
+	var outWait sync.WaitGroup
 
-	// Read
-	from, err := os.Open(fromFilename)
-	defer from.Close()
-	if err != nil {
-		return errors.New("Error opening " + fromFilename + ": " + err.Error())
-	}
-	words := make(chan string)
 	errCh := make(chan error)
-	go func() {
-		seen := make(map[string]bool)
-		scanner := bufio.NewScanner(from)
-		for scanner.Scan() {
-			word := scanner.Text()
-			if validWord(word) {
-				if !seen[word] { // ignore duplicate entries
-					seen[word] = true
-					words <- word
-				}
+
+	in := make(chan wordMember)
+
+	outWait.Add(1)
+	go readFile(inFile, in, errCh, &outWait)
+
+	out := make(chan wordMember)
+	for i := 0; i < 1000; i++ {
+		outWait.Add(1)
+		go func() {
+			defer outWait.Done()
+			for w := range in {
+				w.process()
+				out <- w
 			}
-		}
-		close(words)
-		if err := scanner.Err(); err != nil {
-			errCh <- errors.New("Error reading from " + fromFilename + ": " + err.Error())
-		}
-		close(errCh)
+		}()
+	}
+
+	go func() {
+		outWait.Wait()
+		close(out)
 	}()
 
+	go writeFile(outFile, out, errCh)
+
+	// Get Read and Write errors
+	for err := range errCh {
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+
+}
+
+func readFile(filename string, in chan wordMember, errCh chan error, wg *sync.WaitGroup) {
+
+	defer wg.Done()
+	defer close(in)
+
+	from, err := os.Open(filename)
+	defer from.Close()
+
+	if err != nil {
+		errCh <- fmt.Errorf("Error opening " + filename + ": " + err.Error())
+		return
+	}
+	seen := make(map[string]bool)
+	scanner := bufio.NewScanner(from)
+	for scanner.Scan() {
+		word := scanner.Text()
+		if !seen[word] { // ignore duplicate entries
+			seen[word] = true
+			in <- wordMember{inWord: word}
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		errCh <- fmt.Errorf("Error reading from " + filename + ": " + err.Error())
+	}
+}
+
+func (w *wordMember) process() {
+	if validWord(w.inWord) {
+		w.outWord = "\"" + w.inWord + "\": true, "
+	}
+}
+
+func validWord(candidate string) bool {
+	return len(candidate) > 0 && candidate[0] != '#' && candidate[0] != ' '
+}
+
+func writeFile(filename string, out chan wordMember, errCh chan error) {
 	// Write
-	to, err := os.Create(toFilename)
+	to, err := os.Create(filename)
 	defer to.Close()
 	if err != nil {
-		return errors.New("Error creating " + toFilename + ": " + err.Error())
+		errCh <- fmt.Errorf("Error creating " + filename + ": " + err.Error())
+		return
 	}
 
 	// Write Header
@@ -62,54 +104,33 @@ func streamWords(fromFilename, toFilename string) error {
 	header += "var englishWords = alphabet{"
 	n, err := to.WriteString(header)
 	if err != nil {
-		return errors.New("Error writing header to " + toFilename + ". Wrote " + fmt.Sprint(n) + " bytes before error: " + err.Error())
+		errCh <- fmt.Errorf("Error writing header to " + filename + ". Wrote " + fmt.Sprint(n) + " bytes before error: " + err.Error())
+		return
 	}
 
 	// Write Body
-	for {
-		var open bool
-		var word string
-
-		select {
-		case word, open = <-words: // includes word in map literal
-			n, err = to.WriteString("\"" + word + "\": true, ")
-			if err != nil {
-				return errors.New("Error writing " + word + " to " + toFilename + ". Wrote " + fmt.Sprint(n) + " bytes before error: " + err.Error())
-			}
-			if !open {
-				words = nil // mark for no additional recieves
-			}
-		case err, open = <-errCh: // interrupt
-			if err != nil {
-				break
-			}
-			if !open {
-				errCh = nil // mark for no additional recieves
-			}
+	for word := range out {
+		n, err = to.WriteString(word.outWord)
+		if err != nil {
+			errCh <- fmt.Errorf("Error writing " + word.outWord + " to " + filename + ". Wrote " + fmt.Sprint(n) + " bytes before error: " + err.Error())
+			return
 		}
-
-		if words == nil && errCh == nil {
-			// both channels are nil (therefore they were closed)
-			// must exit now before select blocks forever
-			break
-		}
-
-	}
-
-	if err != nil {
-		return errors.New("Error reading from " + fromFilename + ": " + err.Error())
 	}
 
 	// Write Footer
 	footer := "}"
 	n, err = to.WriteString(footer)
 	if err != nil {
-		return errors.New("Error writing footer to " + toFilename + ". Wrote " + fmt.Sprint(n) + " bytes before error: " + err.Error())
+		errCh <- fmt.Errorf("Error writing footer to " + filename + ". Wrote " + fmt.Sprint(n) + " bytes before error: " + err.Error())
+		return
 	}
 
-	return nil
+	close(errCh)
 }
 
-func validWord(candidate string) bool {
-	return len(candidate) > 0 && candidate[0] != '#' && candidate[0] != ' '
+func main() {
+	err := transcribe("wiki-100k.txt", "words.go")
+	if err != nil {
+		fmt.Println("Error transcribing", err)
+	}
 }
